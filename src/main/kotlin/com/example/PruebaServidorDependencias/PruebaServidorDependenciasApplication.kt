2a6a14556cb8f1dfeb51299cb610bd615e0d8066
Alvaro <alvaro.g.a.99@gmail.com>
2021-01-21 12:56:46 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/PruebaServidorDependencias/PruebaServidorDependenciasApplication.kt
@@ -0,0 +1,11 @@
+package com.example.PruebaServidorDependencias
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PruebaServidorDependenciasApplication
+
+fun main(args: Array<String>) {
+	runApplication<PruebaServidorDependenciasApplication>(*args)
+}
